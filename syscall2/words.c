e9964ec50137aa3abe9fb498bedfc0f6bfa25016
99003510 <rajathpd.pd@ltts.com>
2021-03-04 00:43:12 +0530

    Create words.c

---
+++ syscall2/words.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE * file;
+    char path[100];
+
+    char ch;
+    int characters, words, lines;
+
+    /* read mode 'r' */
+    file = fopen("file1.txt", "r");
+
+
+    /* checks if errors are there */
+    if (file == NULL)
+    {
+        
+		perror("open");
+		exit(1);
+	
+    }
+
+    /*
+     * Logic to count characters, words and lines.
+     */
+    characters = words = lines = 0;
+    while ((ch = fgetc(file)) != EOF)
+    {
+        characters++;
+
+        /* Check new line */
+        if (ch == '\n' || ch == '\0')
+            lines++;
+
+        /* Check words */
+        if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0')
+            words++;
+    }
+
+   
+    /* Print file details*/
+    printf("\n");
+    printf("Total characters = %d\n", characters);
+    printf("Total words      = %d\n", words);
+    printf("Total lines      = %d\n", lines);
+
+
+    /* Close files to release resources */
+    fclose(file);
+
+    return 0;
+}
